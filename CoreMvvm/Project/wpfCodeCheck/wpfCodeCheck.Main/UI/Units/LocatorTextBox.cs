a022202258d89eaa6cbc107ce151e67f2b316d86
DESKTOP-DFB0ECA\shpark <power93266@naver.com>
2024-07-17 13:21:54 +0900

    feat : 디렉토리 검색 Drag Drop 관련 기능이동
    1. DirectoryCompareView 에서 책임지지 않아도될 Drag 기능을 uinits 단위인 LocatorTextBox 자체에 Drag Drop 기능 이동

--- CoreMvvm/Project/wpfCodeCheck/wpfCodeCheck.Main/UI/Units/LocatorTextBox.cs
+++ CoreMvvm/Project/wpfCodeCheck/wpfCodeCheck.Main/UI/Units/LocatorTextBox.cs
@@ -29,5 +29,28 @@ namespace wpfCodeCheck.Main.UI.Units
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(LocatorTextBox), new FrameworkPropertyMetadata(typeof(LocatorTextBox)));
         }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            this.PreviewDragOver += (s, e) => { e.Handled = true; };
+
+            this.Drop += textBlock_Drop;
+
+        }
+        private void textBlock_Drop(object sender, DragEventArgs e)
+        {
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // 파일 경로들을 개행 문자로 결합하여 문자열 생성
+                string filePaths = string.Join("\n", files);
+
+                // TextBox에 파일 경로 문자열 할당
+                this.Text = filePaths;
+            }
+        }
     }
 }
