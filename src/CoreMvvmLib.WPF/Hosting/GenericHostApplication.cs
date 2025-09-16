71c8288ce199e00ba17c901a78e0267a95e0c230
DESKTOP-DFB0ECA\shpark <power93266@naver.com>
2025-09-16 16:59:40 +0900

    feat : App 비정상 종료 시 덤프파일 생성

--- src/CoreMvvmLib.WPF/Hosting/GenericHostApplication.cs
+++ src/CoreMvvmLib.WPF/Hosting/GenericHostApplication.cs
@@ -1,20 +1,18 @@
 ﻿using CoreMvvmLib.WPF.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Threading;
 
 namespace CoreMvvmLib.WPF.Hosting
 {
-    public abstract class GenericHostApplication : Application
+    public abstract class HostApplication : Application
     {
         private CoreDumpHelper.MiniDumpType _dumpType = CoreDumpHelper.MiniDumpType.MiniDumpNormal;
         private bool _canGenerateDump = false;
 
-        protected GenericHostApplication()
+        protected HostApplication()
         {
             Dispatcher.UnhandledException += Dispatcher_UnhandledException;
             Dispatcher.UnhandledExceptionFilter += Dispatcher_UnhandledExceptionFilter;
@@ -22,12 +20,25 @@ namespace CoreMvvmLib.WPF.Hosting
 
         private void Dispatcher_UnhandledExceptionFilter(object sender, DispatcherUnhandledExceptionFilterEventArgs e)
         {
-            throw new NotImplementedException();
+            
         }
+        /// <summary>
+        /// Dump가 발생했을때 덤프를 저장할 위치를 얻어옵니다.
+        /// </summary>
+        /// <returns>Dump의 저장 위치</returns>
+        protected virtual string GetDumpPath()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            string? dirPath = Path.GetDirectoryName(assembly?.Location);
+            string exeName = AppDomain.CurrentDomain.FriendlyName;
+            string dateTime = DateTime.Now.ToString("[yyyy-MM-dd][HH-mm-ss-fff]", CultureInfo.InvariantCulture);
 
+            return $"{dirPath}/[{exeName}]{dateTime}.dmp";
+        }
         private void Dispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
-            throw new NotImplementedException();
+            string dumpPath = GetDumpPath();
+            CoreDumpHelper.CreateMemoryDump(_dumpType, dumpPath);
         }
 
         protected void SetDumpOption(bool canGenerateDump, CoreDumpHelper.MiniDumpType dumpType)
